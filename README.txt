5d484c2b10a1c41f9ca29eb57262d25f86006f82
Ben Jeavons <jeavons@gmail.com>
2014-01-09 14:16:34 -0500

    finished tests, flood control, better naming

--- README.txt
+++ README.txt
@@ -1,22 +1,3 @@
 Two-factor Authentication for Drupal
 
-INSTALLATION:
 
-For default use of the TFA module using the SMS Framework follow these steps.
-
- * Install SMS Framework module and choose an available gateway
- * Install TFA module
- * Add a user field for storing a user's phone number
- * Visit Administration > Configuration > People > Two-factor Authentication to
- choose the user field for use
-
-Optionally you can allow certain user roles to skip the TFA process by giving
-the 'Skip TFA process' permission at Administration > People > Permissions.
-
-Note, if no field is enabled or populated for a user the TFA process will not be
-started.
-
-API:
-
-Consult tfa.api.php for directions on extending TFA with different channels and
-storage and for implementing your own authentication process along with TFA.
--- tfa.api.php
+++ tfa.api.php
@@ -14,148 +14,37 @@
  */
 
 /**
- * There are two ways to integrate your module with TFA. You can (1) provide
- * methods for communicating TFA codes and storing the address field and (2)
- * alter the functions involved with beginning two-factor authentication for a
- * user.
+ * Define TFA plugins.
  *
- * To provide your own code communication channel implement hook_tfa_api() and
- * set attributes as necessary. See the next section for details.
+ * This hook is required to use your own TFA plugin.
  *
- * To alter or ammend the TFA process implement hook_tfa_processes_alter().
- */
-
-/**
- * Define TFA communication channel and address storage.
+ * A plugin must extend the TfaBasePlugin class and may implement one or more of
+ * the TFA plugin interfaces.
  *
- * This hook is required to provide a TFA channel for login code communication
- * or to provide custom storage of a address (e.g. phone number) for an account.
+ * Note, user-defined plugin classes must be available to the Drupal registry
+ * for loading.
  *
  * @return
- *   An array of information about the module's methods for TFA integration.
+ *   Keyed array of information about the plugin for TFA integration.
  *
- *   Required attributes are:
+ *   Required key:
  *
- *    - 'title'
- *        Human-readable title of the communication channel.
- *    - 'send callback'
- *        Function to call for login code transfer. Arguments are $account (a
- *        fully loaded user object being authenticated), $code (the login code,
- *        string), $message (message to accompany code, string). Must return
- *        boolean TRUE or FALSE.
- *    - 'address callback'
- *        Function to call for a unique address for the account being
- *        authenticated. Arguments are $account, a fully loaded user object.
- *        Must return a non-empty variable or FALSE.
+ *    - 'machine_name'
+ *      Unique machine name identifying the plugin.
+ *
+ *    With required sub-array containing:
+ *
+ *    - 'name'
+ *       Human-readable name of the plugin.
+ *    - 'class'
+ *       Class name of the plugin.
  */
 function hook_tfa_api() {
   return array(
-    'title' => t('TFA Example Channel'),
-    'send callback' => '_tfa_example_send_code',
-    'address callback' => '_tfa_example_lookup_address',
+    'my_tfa_plugin' => array(
+      'name'  => 'My TFA plugin',
+      'class' => 'MyTfaPlugin',
+    ),
   );
 }
 
-/**
- * Ammend or alter the TFA authentication process.
- *
- * Implement hook_tfa_processes_alter() to ammend the order and definition of
- * functions to invoke while executing two-factor authentication for an account.
- *
- * Each process defined by a hook_tfa_processes_alter() must be included in the
- * running PHP process. Each function is passed the user object account being
- * executed on and an alterable array of contextual information about the
- * current TFA process. Core TFA will include the 'tfa_code' element in the
- * context to establish where in the TFA process the user resides. A TFA code is
- * an array containing elements string 'code', 'accepted' which evaluates to
- * true or false, and integer 'created'.
- *
- * Note, there is no flood protection when not using TFA's default code process.
- *
- * @see tests/tfa_test.module for more example usuage.
- */
-function hook_tfa_processes_alter(&$processes) {
-  $processes[] = '_tfa_example_process_callback';
-}
-
-/**
- * Example TFA API implementation to use email as communication channel.
- */
-
-/**
- * Implements hook_tfa_api().
- */
-function email_tfa_tfa_api() {
-  return array(
-    'title' => t('Email code'),
-    'send callback' => 'email_tfa_send',
-    'address callback' => 'email_tfa_get_address',
-  );
-}
-
-/**
- * Address collector for Email TFA.
- */
-function email_tfa_get_address($account) {
-  return $account->mail;
-}
-
-/**
- * Send method for Email TFA.
- */
-function email_tfa_send($account, $code, $message) {
-  $params = array(
-    'code' => $code,
-    'message' => $message,
-  );
-  $from = variable_get('site_mail', 'admin@example.com');
-  $result = drupal_mail('email_tfa', 'tfa_send', $account->mail, user_preferred_language($account), $params, $from, TRUE);
-  return $result['result'];
-}
-
-/**
- * Implements hook_mail().
- */
-function email_tfa_mail($key, &$message, $params) {
-  if ($key == 'tfa_send') {
-    $message['subject'] = t('Login code from @site-name', array('@site-name' => variable_get('site_name', 'Drupal')));
-    $message['body'][] = t('@message', array('@message' => $params['message']));
-    $message['body'][] = t('@code', array('@code' => $params['code']));
-  }
-}
-
-/**
- * Example TFA process chain alter to skip TFA code verification for users with
- * the administrator role.
- *
- * Note, the following code is redundant with core TFA permissions but is here
- * for illustration.
- */
-
-/**
- * Implements hook_tfa_process_alter to alter the TFA process chain.
- *
- * @param $processes Array of functions in the TFA process chain.
- */
-function admin_user_tfa_processes_alter(&$processes) {
-  // Push this process callback before default TFA process to ensure it is run
-  // before TFA redirects request to code entry form.
-  array_unshift($processes, 'admin_user_skip_tfa');
-}
-
-/**
- * TFA process callback for admin_user example.
- *
- * @param $account User account object.
- * @param $context Alterable context about the running TFA process.
- *
- * @return string Constants TFA_PROCESS_LOGIN to denote that user may skip any
- * remaining TFA processes and authenticate or TFA_PROCESS_NEXT so next process
- * is invoked.
- */
-function admin_user_skip_tfa($account, &$context) {
-  if (array_key_exists(3, $account->roles)) {
-    return TFA_PROCESS_LOGIN;
-  }
-  return TFA_PROCESS_NEXT;
-}
